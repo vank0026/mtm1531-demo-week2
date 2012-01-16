8280d16003bb88aa013d18d931c0c935461b2297
vank0026 <roger.van.koughnett@gmail.com>
2012-01-16 14:03:25 -0500

    end of class lesson

--- forms.php
+++ forms.php
@@ -1,9 +1,15 @@
 <?php
 
+  
+//on regular computers, we get an error.  On Edumedia however, you will NEVER see error messages, unless...
+//error_reporting(-1);
+//ini_set('display_errors', 'on');
+
+
 var_dump($_POST);
 
 ?>
-
+  
 <!DOCTYPE HTML>
 <html>
 <head>
@@ -20,6 +26,15 @@ var_dump($_POST);
 	<label for="moon">How Many Moons?</label>
 	<input type="number" id="moon" name="moon">
 	
+	<label for="galaxy">What Galaxy?</label>
+	<select id="galaxy" name="galaxy">
+		<option value="m">Milky Way Galaxy</option><!-- the value is sent to the server as m, rahter than as Milky Way Galaxy, .: more useable -->
+		<option value="a">Andromeda Galaxy</option>
+		<option value="t">Tadpole Galaxy</option>
+		<option value="s">Sunflower Galaxy</option>
+		<option value="c">Cartwheel Galaxy</option>
+	</select>
+	
 	<button type="submit">Submit</button>
 </form>
 
@@ -27,10 +42,47 @@ var_dump($_POST);
 <?php if (!empty($_POST["planet"]) && !empty($_POST["moon"])) : ?>
 
 	<p><strong>Does <?php echo $_POST["planet"]; ?> really have <?php echo $_POST["moon"]; ?> moons?  <i>Really?</i></strong></p>
+	<p>Always remember, Men Very Easily Make Juggs Serve Usefull Nocturnal Purposes!</p>
 
-	<?php if ($_POST["planet"] == "Saturn") : ?>
-	<p>That's some mad bling.</p>
+	<?php if ($_POST["planet"] == "Mercury") : ?>
+	<p>That's one really hot planet.</p>
+
+	<?php elseif ($_POST["planet"] == "Venus") : ?>
+	<p>The godess of love was way better than Minerva, that cow.</p>
+
+	<?php elseif ($_POST["planet"] == "Earth") : ?>
+	<p>That's where we live!</p>
+
+	<?php elseif ($_POST["planet"] == "Mars") : ?>
+	<p>There is no proof that they visited in the 40's.  Honest.</p>
+
+	<?php elseif ($_POST["planet"] == "Jupiter") : ?>
+	<p>The red spot is thrice the width of Earth.  Thats huge!</p>
+
+	<?php elseif ($_POST["planet"] == "Saturn") : ?>
+	<p>That's way too many rings for anyone's comfort.</p>
+
+	<?php elseif ($_POST["planet"] == "Uranus") : ?>
+	<p>That's not Myanus! Haha!</p>
+	
+	<?php elseif ($_POST["planet"] == "Neptune") : ?>
+	<p>Silly person, that's not the King of the Sea, but a planet.</p>
+
+	<?php elseif ($_POST["planet"] == "Pluto") : ?>
+	<p>That's not really a planet anymore!</p>
+	
+	<?php else : ?>
+		<p>That's not a real planet.  Try again.</p>
+
 	<?php endif; ?>
+	
+	<?php 
+	
+		$moon_diff = $_POST["moon"] - 1;
+	
+	?>
+	
+	<p>You think <?php echo $_POST["planet"]; ?> has <?php echo $moon_diff; ?> more moons than Earth.</p>
 
 <?php endif; ?>
 
--- index.php
+++ index.php
@@ -1,11 +1,23 @@
 <!DOCTYPE HTML>
 <html>
 <head>
-<meta charset="utf-8">
-<title>Query Strings</title>
+	<meta charset="utf-8">
+	<title>Query Strings</title>
 </head>
 <body>
 
+<?php 
+	
+	echo $_GET["name"];
+	echo " ";
+	echo $_GET["lastname"];
+
+?>
+
+<a href="name.php?name=Roger">Roger</a>
+<a href="name.php?name=Guy">Guy</a>
+<a href="name.php?name=Buddy">Buddy</a>
+<a href="name.php?name=Friend">Friend</a>
 
 
 </body>
